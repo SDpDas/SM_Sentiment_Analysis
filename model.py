3fe70e9777d3c3d3e11b8fd7c27d84167199cdf9
SDpDas <sd0706@srmist.edu.in>
2024-09-15 05:20:53 +0530

    fixed

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
 # Load the saved model and vectorizer
-loaded_model = pickle.load(open('trained_model.sav', 'rb'))
+loaded_model = pickle.load(open('new_trained_model.sav', 'rb'))
 vectorizer = pickle.load(open('vectorizer.sav', 'rb'))
 
 # Define the preprocessing function used during training
@@ -29,7 +29,9 @@ X_new = vectorizer.transform(preprocessed_tweets)
 # Make predictions
 predictions = loaded_model.predict(X_new)
 
+#1 for negative and 2 for positive sentiment
+
 # Print predictions
 for tweet, prediction in zip(tweets, predictions):
-    sentiment = 'Positive' if prediction == 1 else 'Negative'
-    print(f"Tweet: {tweet}\nPrediction: {sentiment}\n")
+    sentiment = 'Positive' if prediction == 2 else 'Negative'
+    print(f"Pred_value: {prediction}\nTweet: {tweet}\nPrediction: {sentiment}\n")
